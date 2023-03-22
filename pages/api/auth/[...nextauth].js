986a8b623ae30bb6aeb6b0202fca352b32823f94
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-03-22 15:21:38 +0530

    provider changed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import GithubProvider from "next-auth/providers/github"
+
+
 
 export default NextAuth({
-  Providers: [
-    Providers.Github({
+  providers: [
+    GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
